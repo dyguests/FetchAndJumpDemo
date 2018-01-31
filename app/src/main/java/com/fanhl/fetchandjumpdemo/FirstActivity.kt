93495a8776640f93393cf71ea1a162762ea3a889
fanhl <fanhonglin@klicen.com>
2018-01-31 15:31:58 +0800

    view model 生命周期

--- app/src/main/java/com/fanhl/fetchandjumpdemo/FirstActivity.kt
+++ app/src/main/java/com/fanhl/fetchandjumpdemo/FirstActivity.kt
@@ -1,5 +1,7 @@
 package com.fanhl.fetchandjumpdemo
 
+import android.arch.lifecycle.ViewModelProviders
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,6 +15,17 @@ class FirstActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             SecondActivity.launch(this@FirstActivity)
+            val viewModel = ViewModelProviders.of(this).get(SecondActivity.LiveDataViewModel::class.java)
+            object : AsyncTask<Void, Void, String>() {
+                override fun doInBackground(vararg params: Void?): String {
+                    Thread.sleep(2000)
+                    return "${System.currentTimeMillis()}"
+                }
+
+                override fun onPostExecute(result: String?) {
+                    viewModel.text.value = result
+                }
+            }.execute()
         }
     }
 }
--- app/src/main/java/com/fanhl/fetchandjumpdemo/SecondActivity.kt
+++ app/src/main/java/com/fanhl/fetchandjumpdemo/SecondActivity.kt
@@ -1,26 +1,30 @@
 package com.fanhl.fetchandjumpdemo
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: LiveDataViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        viewModel = ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
+
+        viewModel.text.observe(this, Observer {
+            tv_text.text = it
+        })
     }
 
     companion object {
@@ -33,5 +37,7 @@ class SecondActivity : AppCompatActivity() {
         }
     }
 
-    class LiveDataViewModel : ViewModel()
+    class LiveDataViewModel : ViewModel() {
+        val text = MutableLiveData<String>()
+    }
 }
